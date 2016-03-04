80438c907065919f76403099367882f0982a3462
ecly <elyne@live.dk>
2016-03-04 19:08:46 +0100

    Find all prime number integer and print to console.

--- Primes/Program.cs
+++ Primes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace Primes
     {
         static void Main(string[] args)
         {
+            var primes = new List<int> { 2 };
+
+            for (int i = 3; i < int.MaxValue; i++)
+            {
+                var isPrime = true;
+                foreach (int j in primes)
+                {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    primes.Add(i);
+                    Console.WriteLine("Found prime number: " + i);
+                }
+            }
         }
     }
 }
